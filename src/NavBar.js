2fcfbbe81b5b73e8d66ab1b3506bc800cac2f900
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-04-03 14:12:21 +0100

    add the navbar component

---
+++ src/NavBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { Switch } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles/NavBarStyle";
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <IconButton className={classes.menuButton} color='inherit'>
+            <span>🏳</span>
+          </IconButton>
+          <Typography className={classes.title} variant='h6' color='inherit'>
+            DEV-PRO
+          </Typography>
+          <Switch />
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder='Search...'
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
